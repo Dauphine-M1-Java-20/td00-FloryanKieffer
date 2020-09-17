474270f22ba02da39b67c8e9d868dfaca7a5bd98
FloryanKieffer <floryan@mbp-de-floryan.home>
2020-09-17 15:50:29 +0200

    initial import

---
+++ td00/src/main/java/fr/dauphine/ja/kiefferfloryan/td00/PrimeCollection.java
@@ -0,0 +1,49 @@
+package fr.dauphine.ja.kiefferfloryan.td00;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Bonjour!
+ *
+ */
+public class PrimeCollection 
+{
+	ArrayList<Integer> numbers;
+	
+	public PrimeCollection() {
+		this.numbers = new ArrayList<Integer>();
+	}
+	
+	public void initRandom(int n, int m) {
+		for(int i = 0; i<n;i++) {
+			int alea = (int)(Math.random()*m);
+			System.out.println(alea);
+			this.numbers.add(alea);
+		}
+	}
+	
+	public boolean isPrime(int p) {
+		for(int i = 2; i<p/2;i++) {
+			if(p%i==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public void printPrime() {
+		for(int i = 0; i<this.numbers.size();i++) {
+			if(this.isPrime(this.numbers.get(i))==true) {
+				System.out.println(this.numbers.get(i));
+			}
+		}
+	}
+	
+    public static void main( String[] args )
+    {
+        PrimeCollection a = new PrimeCollection();
+        a.initRandom(100, 1000);
+        a.printPrime();
+    }
+}
